3cf485d9f58219f696035194c66969cd4f134020
bigqnv <153248753+bigqnv@users.noreply.github.com>
2024-10-25 21:34:44 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HeaderLayout from "./components/Layout/HeaderLayout.tsx";
+
+function App() {
+  return (
+    <>
+      <HeaderLayout />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/HeaderLayout.tsx
@@ -0,0 +1,9 @@
+import { Header } from "antd/es/layout/layout";
+
+export default function HeaderLayout() {
+  return (
+    <>
+      <Header className="w-full p-0 m-0 reset"></Header>
+    </>
+  );
+}
